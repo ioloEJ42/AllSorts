aef50cc3abb4a41b96dd327a80851dad2307f2ab
ioloEJ42 <ioloevans42@gmail.com>
2024-10-28 20:43:57 +0000

    Updated Bubblesort orange highlighting

--- sorting-visualizer/src/algorithms/bubblesort.ts
+++ sorting-visualizer/src/algorithms/bubblesort.ts
@@ -8,29 +8,38 @@ const bubbleSortLogic = async (
   delay: () => Promise<void>
 ) => {
   const n = array.length;
-
   for (let i = 0; i < n - 1; i++) {
     let swapped = false;
-
     for (let j = 0; j < n - i - 1; j++) {
+      // Set comparing state for current pair
       array[j].isComparing = true;
       array[j + 1].isComparing = true;
       updateArray([...array]);
-
       await delay();
 
       if (array[j].height > array[j + 1].height) {
+        // Swap elements
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
         swapped = true;
+        updateArray([...array]);
+        await delay(); // Add delay after swap to make it visible
       }
 
+      // Reset comparing state after comparison and potential swap
       array[j].isComparing = false;
       array[j + 1].isComparing = false;
       updateArray([...array]);
+
+      // Only add delay if not at the end of inner loop
+      if (j < n - i - 2) {
+        await delay();
+      }
     }
-
+    
+    // Mark element as sorted
     array[n - 1 - i].isSorted = true;
-
+    updateArray([...array]);
+    
     // If no swapping occurred, array is sorted
     if (!swapped) {
       // Mark remaining elements as sorted
@@ -40,7 +49,7 @@ const bubbleSortLogic = async (
       break;
     }
   }
-
+  
   // Ensure first element is marked as sorted
   array[0].isSorted = true;
   updateArray([...array]);
